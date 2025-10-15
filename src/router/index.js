e57ff8ec6435807deaa5407e58bed022f8d75871
alhxmbs <alhexmbs@gmail.com>
2025-10-15 17:35:32 -0500

    store de pinia listo - falta implementar

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from '../stores/authStore';
+
 import AuthView from "../views/AuthView.vue";
 import PendienteEvaluacionView from "../views/PendienteEvaluacionView.vue";
 import ValidacionPersonalView from "../views/ValidacionPersonalView.vue"
@@ -6,37 +8,42 @@ import DoctorsAvailabilityView from "../views/DoctorsAvailabilityView.vue";
 import DoctorsAppointmentView from "../views/DoctorsAppointmentView.vue";
 import ChatView from "../views/ChatView.vue";
 
-/* Implementar en la autenticación cuando se implemente
-const router = [
-    { path: '/', redirect: '/auth' },
+// Implementar en la autenticación cuando se implemente
+const routes = [
+    { path: '/auth', component: AuthView },
     {
-        path: '/auth',
-        name: 'auth',
-        component: AuthView,
+        path: '/',
+        component: PendienteEvaluacionView,
         children: [
             { path: 'validacion_personal', component: ValidacionPersonalView },
             { path: 'pendiente', component: PendienteEvaluacionView },
+            { path: 'disponibilidad_medico', component: DoctorsAvailabilityView },
+            { path: 'citas_medico', component: DoctorsAppointmentView },
+            { path: 'chat_medico', component: ChatView },
         ],
         meta: { requiresAuth: true }
     },
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 
 router.beforeEach((to, from, next) => {
-  const authStore = useAuthStore()
-  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-    next('/login')
-  } else {
-    next()
-  }
+    const authStore = useAuthStore()
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+        next('/auth')
+    } else {
+        next()
+    }
 })
 
-*/
+export default router
 
+
+
+/*
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -74,3 +81,4 @@ const router = createRouter({
 });
 
 export default router;
+*/
